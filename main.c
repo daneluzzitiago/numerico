85cdd8ec18425048dd21a0cd33e115b17fac1997
Tiago Daneluzzi <daneluzzitiago@gmail.com>
2019-10-10 19:18:13 -0300

    Matrix structure
    Make a matrix struct and node struct to link info.

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+typedef struct Node{
+	int i;
+	int k;
+	int key;
+	*Node vertical_next;
+	*Node horizontal_next;
+}Node;
+
+typedef struct Matrix{
+	**Node data;
+}Matrix;
+
 int main(void){
 	printf("Hello world\n");
 
